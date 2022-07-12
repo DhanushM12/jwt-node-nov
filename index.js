1b08cb2e37c2056e036262a8052f71dafe22f496
DhanushM12 <dhanushmukhi@gmail.com>
2022-07-12 20:09:37 +0530

    added verify token

--- index.js
+++ index.js
@@ -23,6 +23,34 @@ app.post('/tokenGeneration', (req, res) => {
       });
 })
 
+app.post('/verifyToken', takeToken, (req, res) => {
+    jwt.verify(req.token, 'secretkey', function(err, data){
+        if(err){
+            res.sendStatus(403)
+        }
+        else{
+            res.json({
+                message: 'user access granted',
+                data
+            })
+        }
+    })
+})
+
+// middleware
+function takeToken(req, res, next){
+    const bearerHeader = req.headers['authorization'];
+    if(bearerHeader !== undefined){
+        const bearer = bearerHeader.split(' ');
+        const bearerToken = bearer[1];
+        req.token = bearerToken;
+        next();
+    }
+    else{
+        res.sendStatus(403);
+    }
+}
+
 app.listen(port, function(err){
     if(err){
         console.log(`Error in running the server: ${err}`);
